907de7a8aeab4d59f417dc2c251423831fd6695f
carolhn <nunescaroline905@gmail.com>
2025-01-30 21:26:31 -0300

    feat: [EC-019] create review router

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,6 +1,7 @@
 import Brand from './brand';
 import Category from './category';
 import Product from './product';
+import Review from './review';
 import User from './user';
 
-export { Brand, Category, Product, User };
+export { Brand, Category, Product, Review, User };
