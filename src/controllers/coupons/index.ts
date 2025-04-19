1c065e5ca96672600f2e86cb776109d19bd4cfa6
carolhn <nunescaroline905@gmail.com>
2025-04-19 18:21:54 -0300

    feat: [EC-023] Add new functionality  createCoupon

--- src/controllers/coupons/index.ts
+++ src/controllers/coupons/index.ts
@@ -1,11 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
+import { Coupon } from '../../model/index';
 
 export const createCoupon = asyncHandler(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { code, discount, endDate, startDate } = req.body;
+
+    const couponExists = await Coupon.findOne({
+      code,
+    });
+
+    if (couponExists) {
+      throw new Error('Product already exists');
+    }
+
+    if (isNaN(discount)) {
+      throw new Error('Discount value must be a number');
+    }
+
+    const coupon = await Coupon.create({
+      code,
+      startDate,
+      endDate,
+      discount,
+      user: req.body.user.id,
+    });
+
     res.json({
       status: 'success',
       message: 'Coupon created successfully',
+      coupon,
     });
   },
 );
--- src/routes/coupons/index.ts
+++ src/routes/coupons/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { createCoupon } from '../../controllers/index';
+import { isAuthenticated } from '../../middlewares/auth';
 
 const couponRoutes = Router();
 
-couponRoutes.post('/', createCoupon as any);
+couponRoutes.post('/', isAuthenticated, createCoupon as any);
 
 export default couponRoutes;
