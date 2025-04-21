eab256424fc262651b9286cc90ce789064bbdd76
carolhn <nunescaroline905@gmail.com>
2025-04-21 11:27:10 -0300

    feat: [EC-025] add isAdmin in routes

--- src/routes/coupons/index.ts
+++ src/routes/coupons/index.ts
@@ -7,13 +7,14 @@ import {
   updatedCoupon,
 } from '../../controllers/index';
 import { isAuthenticated } from '../../middlewares/auth';
+import isAdmin from '../../middlewares/isAdmin';
 
 const couponRoutes = Router();
 
-couponRoutes.post('/', isAuthenticated, createCoupon as any);
+couponRoutes.post('/', isAuthenticated, isAdmin, createCoupon as any);
 couponRoutes.get('/listCoupons', isAuthenticated, getAllCoupons as any);
 couponRoutes.get('/:id', isAuthenticated, getSingleCoupon as any);
-couponRoutes.put('/:id', isAuthenticated, updatedCoupon as any);
-couponRoutes.delete('/:id', isAuthenticated, deleteCoupon as any);
+couponRoutes.put('/:id', isAuthenticated, isAdmin, updatedCoupon as any);
+couponRoutes.delete('/:id', isAuthenticated, isAdmin, deleteCoupon as any);
 
 export default couponRoutes;
--- src/routes/product/index.ts
+++ src/routes/product/index.ts
@@ -7,13 +7,14 @@ import {
   updateProduct,
 } from '../../controllers/index';
 import { isAuthenticated } from '../../middlewares/auth';
+import isAdmin from '../../middlewares/isAdmin';
 
 const productRoutes = Router();
 
-productRoutes.post('/create', isAuthenticated, createProduct as any);
+productRoutes.post('/create', isAuthenticated, isAdmin, createProduct as any);
 productRoutes.get('/list', getProducts as any);
 productRoutes.get('/:id', getProductById as any);
-productRoutes.put('/:id', isAuthenticated, updateProduct as any);
-productRoutes.delete('/:id', isAuthenticated, deleteProduct as any);
+productRoutes.put('/:id', isAuthenticated, isAdmin, updateProduct as any);
+productRoutes.delete('/:id', isAuthenticated, isAdmin, deleteProduct as any);
 
 export default productRoutes;
